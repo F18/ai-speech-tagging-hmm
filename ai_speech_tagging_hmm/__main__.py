01ce339eed4917833865b0a2cef461565631cfe0
F18 <F18@users.noreply.github.com>
2023-11-26 11:26:38 +0000

    ✅ Ready to clone and code.

---
+++ ai_speech_tagging_hmm/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for ai_speech_tagging_hmm."""
+
+from ai_speech_tagging_hmm.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- ai_speech_tagging_hmm/base.py
+++ ai_speech_tagging_hmm/base.py
@@ -1,7 +1,7 @@
 """
-project_name base module.
+ai_speech_tagging_hmm base module.
 
-This is the principal module of the project_name project.
+This is the principal module of the ai_speech_tagging_hmm project.
 here you put your main classes and objects.
 
 Be creative! do whatever you want!
@@ -14,4 +14,4 @@ and then choose `flask` as template.
 """
 
 # example constant variable
-NAME = "project_name"
+NAME = "ai_speech_tagging_hmm"
